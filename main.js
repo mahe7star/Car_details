443a624e63bd1aed59fd274b202279d344c3258a
mahe7star <mp1995singh@gmail.com>
2018-09-11 09:33:13 +0530

    minor fix

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function changeImage(color){
     const element = $('#car-img');
-    element.attr("src",`/assets/images/color/${color}.jpg`);
+    element.attr("src",`assets/images/color/${color}.jpg`);
     $('.color-name').text(color);
 }
 
