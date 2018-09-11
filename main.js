91aac94dcc193af4faa32de13eb74fb956891c8f
mahe7star <mp1995singh@gmail.com>
2018-09-11 11:38:30 +0530

    fices

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-function changeImage(color){
+function changeImage(color, curr){
     const element = $('#car-img');
     element.attr("src",`assets/images/color/${color}.jpg`);
     $('.color-name').text(color);
+    $('.color-cirles').removeClass('active');
+    $(curr).addClass('active');
 }
 
